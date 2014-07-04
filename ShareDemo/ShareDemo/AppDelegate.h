e9532490b5dee8e2b1199b9e7da0aeaa963a60d6
luotongyi <3a:23:7b:1d:ce:8d:6e:d3:f5:2e:48:16:48:a6:dc:a7>
2014-07-04 17:32:30 +0800

    project

---
+++ ShareDemo/ShareDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShareDemo
+//
+//  Created by luotongyi on 14-7-4.
+//  Copyright (c) 2014年 luotongyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareDemo/ShareDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ShareDemo
+//
+//  Created by luotongyi on 14-7-4.
+//  Copyright (c) 2014年 luotongyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
